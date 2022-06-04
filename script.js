420f4605f76f1265e9fd1e896b5690c3bc9f02fc
Nabeel Mohammad Rizwan <nabeel.rizwan123@ymail.com>
2022-06-05 01:27:44 +0530

    commit

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ let taskbar = document.getElementsByClassName("taskbar")[0]
 let start = document.getElementsByClassName("start")[0]
 let notification = document.getElementsByClassName("notification")[0]
 let right = document.getElementsByClassName("right")[0]
+let nabeel = document.getElementsByClassName("nabeel")[0]
+let windows = document.getElementsByClassName("window")[0]
+
+nabeel.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(windows.style.opacity == 0){
+        windows.style.opacity = 1;
+    }
+    else{
+        windows.style.opactiy = 0;
+    }
+});
+
+windows.addEventListener("click",()=>{
+    console.log("hello")
+    if (windows.style.opacity == 1){
+        windows.style.opacity = 0;
+    }
+})
 
 taskbar.addEventListener("click", ()=>{
     console.log("clicked");
